7d06c9adfdd0a0f648a7e68c95c7b4b3a0e0db0e
sHesl <samuel.heslop.dev@gmail.com>
2021-06-30 19:34:16 +0100

    Please setup for local development

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/thought-machine/falco-probes
+
+go 1.16
+
+require golang.org/x/exp/errors v0.0.0-20210625193404-fa9d1d177d71
---
+++ pkg/fmtme/fmtme.go
@@ -0,0 +1,6 @@
+package fmtme
+
+import "golang.org/x/exp/errors"
+
+var FmtMe = "temporary package to test plz fmt-all command"
+var ImportMe = errors.As
