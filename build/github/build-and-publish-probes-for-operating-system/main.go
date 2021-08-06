8fb054a9d989e1844c8abe39dd7ef5d81fd81b2e
lemanie <14907551+lemanie@users.noreply.github.com>
2021-08-06 11:55:27 +0100

    Add check to build workflow for isAlreadyMirrored to skip building already published probes (#32)
    * update log message from IsAleadyMirrored
    
    * added in IsAlreadyMirrored check, moved the driver version sourcing & storage to before the loop so it happens once not 67 times
    
    * remove the test-is-already-mirrored workflow as this is now going to be part of the build workflow
    
    * fix lint error with type falcoVersion
    
    Co-authored-by: lemanie <lemanie@users.noreply.github.com>

--- build/github/build-and-publish-probes-for-operating-system/main.go
+++ build/github/build-and-publish-probes-for-operating-system/main.go
@@ -15,16 +15,21 @@ import (
 
 var log = logging.Logger
 
-// FalcoVersions represents the list of Falco versions to build eBPF probes for the given operating system. We're only interested in building the versions
+// FalcoVersionNames represents the list of Falco versions to build eBPF probes for the given operating system. We're only interested in building the versions
 // that diversify our support for Falco driver versions as they maintain compatibility between different Falco versions.
 // Note: We can only support 0.28.1+ at the moment as it seems like the falco-driver-loader script changed in an incompatible way between 0.26 and 0.28.1.
 // TODO: To fix this, we could just source the driver loader script from 0.28.1 and reuse that instead of the script bundled w/ each falco-driver-loader.
-var FalcoVersions = []string{
+var FalcoVersionNames = []string{
 	"0.24.0", // falco-driver-version: 85c88952b018fdbce2464222c3303229f5bfcfad
 	"0.25.0", // falco-driver-version: ae104eb20ff0198a5dcb0c91cc36c86e7c3f25c7
 	"0.26.0", // falco-driver-version: 2aa88dcf6243982697811df4c1b484bcbe9488a2
 	"0.28.1", // falco-driver-version: 5c0b863ddade7a45568c0ac97d037422c9efb750
 	"0.29.1", // falco-driver-version: 17f5df52a7d9ed6bb12d3b1768460def8439936d
+}
+
+type falcoVersion struct {
+	Name   string
+	Driver string
 }
 
 type opts struct {
@@ -41,6 +46,15 @@ func main() {
 
 	cli := docker.MustClient()
 	ghReleases := ghreleases.MustGHReleases(&opts.GHReleases)
+
+	log.Info().Msg("Getting list of falco drivers")
+	FalcoVersions, err := getFalcoDrivers(cli, FalcoVersionNames)
+	if err != nil {
+		log.Fatal().Err(err).Msg("could not get falco drivers")
+	}
+	log.Info().
+		Int("amount", len(FalcoVersions)).
+		Msg("Got falco drivers")
 
 	log.Info().
 		Str("operating_system", opts.Positional.OperatingSystem).
@@ -65,7 +79,7 @@ func main() {
 	parallelFns := []func() error{}
 	for _, kernelPackageName := range kernelPackageNames {
 		parallelFns = append(parallelFns, func() error {
-			return buildProbesForKernelPackageName(
+			return process1KernelPackage(
 				cli,
 				ghReleases,
 				operatingSystem,
@@ -80,17 +94,17 @@ func main() {
 	handleErrs(errs)
 }
 
-func buildProbesForKernelPackageName(
+func process1KernelPackage(
 	dockerCli *docker.Client,
 	repo repository.Repository,
 	operatingSystem operatingsystem.OperatingSystem,
 	kernelPackageName string,
-	falcoVersions []string,
+	falcoVersions []falcoVersion,
 ) error {
+	// Get the required package specific values
 	log.Info().
-		Str("kernel_package", kernelPackageName).
-		Msg("Getting kernel package")
-
+		Str("kernel_package_name", kernelPackageName).
+		Msg("Getting kernel_package for")
 	kernelPackage, err := operatingSystem.GetKernelPackageByName(kernelPackageName)
 	if err != nil {
 		return fmt.Errorf("could not get kernel package '%s': %w", kernelPackageName, err)
@@ -99,33 +113,81 @@ func buildProbesForKernelPackageName(
 		kernelPackage.KernelSources,
 		kernelPackage.KernelConfiguration,
 	)
-
+	probeName := kernelPackage.ProbeName() + ".o"
 	log.Info().
 		Str("kernel_package", kernelPackage.Name).
-		Msg("Got kernel package")
+		Str("probe_name", probeName).
+		Msg("Got kernel_package")
 
 	for _, falcoVersion := range falcoVersions {
+		// Check if probe is already mirrored to our repository & doesn't require building
 		log.Info().
-			Str("kernel_package", kernelPackage.Name).
-			Str("falco_version", falcoVersion).
-			Msg("Building Falco eBPF probe")
-
-		falcoDriverVersion, probePath, err := falcodriverbuilder.BuildEBPFProbe(
-			dockerCli,
-			falcoVersion,
-			operatingSystem,
-			kernelPackage,
-		)
+			Str("driver", falcoVersion.Driver).
+			Str("probe_name", probeName).
+			Msg("Checking whether probe is built & published")
+		alreadyPublished, err := repo.IsAlreadyMirrored(falcoVersion.Driver, probeName)
 		if err != nil {
-			return fmt.Errorf("could not build eBPF probe for '%s': %w", kernelPackage.Name, err)
+			log.Error().Err(err).Msg("") // will just be logged as if probe is unfound it makes sense to try to build & publish it
 		}
 
-		if err := repo.PublishProbe(falcoDriverVersion, probePath); err != nil {
-			return fmt.Errorf("could not publish probe: %w", err)
+		if alreadyPublished {
+			log.Info().
+				Str("driver", falcoVersion.Driver).
+				Str("probe_name", probeName).
+				Msg("Skipping, probe is already built & published")
+		} else {
+			// Build unfound probe
+			log.Info().
+				Str("driver", falcoVersion.Driver).
+				Str("probe_name", probeName).
+				Msg("Not found, probe will now be built")
+			builtDriverVersion, probePath, err := falcodriverbuilder.BuildEBPFProbe(
+				dockerCli,
+				falcoVersion.Name,
+				operatingSystem,
+				kernelPackage,
+			)
+			if err != nil {
+				return fmt.Errorf("could not build probe for '%s': %w", kernelPackage.Name, err)
+			}
+
+			// Publish unfound probe
+			log.Info().
+				Str("driver", builtDriverVersion).
+				Str("probe_path", probePath).
+				Msg("Probe built, now publishing probe")
+			if err := repo.PublishProbe(builtDriverVersion, probePath); err != nil {
+				return fmt.Errorf("could not publish probe: %w", err)
+			}
 		}
 	}
 
 	return nil
+}
+
+func getFalcoDrivers(dockerCli *docker.Client, FalcoVersionNames []string) ([]falcoVersion, error) {
+	var FalcoVersions []falcoVersion
+
+	for _, falcoVersionName := range FalcoVersionNames {
+		log.Info().
+			Str("name", falcoVersionName).
+			Msg("Getting driver for")
+		falcoDriverBuilderImg, err := falcodriverbuilder.BuildImage(dockerCli, falcoVersionName)
+		if err != nil {
+			return FalcoVersions, fmt.Errorf("could not get falco_driver_builder_image for %s:%w", falcoVersionName, err)
+		}
+		driver, err := falcodriverbuilder.GetDriverVersion(dockerCli, falcoDriverBuilderImg)
+		if err != nil {
+			return FalcoVersions, fmt.Errorf("could not get driver for %s:%w", falcoDriverBuilderImg, err)
+		}
+		log.Info().
+			Str("name", falcoVersionName).
+			Str("driver", driver).
+			Msg("Got driver")
+		FalcoVersions = append(FalcoVersions, falcoVersion{falcoVersionName, driver})
+	}
+
+	return FalcoVersions, nil
 }
 
 func handleErrs(errs []error) {
